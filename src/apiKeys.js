917c8e35bcbf5d27cc92f61954a60771744fc2d3
Jayesh Patil <jayeshpatil9@hotmail.com>
2025-01-31 11:40:17 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,6 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-    key: "YOU API KEYS",
+    key: "YOUR API KEYS",
     base: "https://api.openweathermap.org/data/2.5/",
   };
   
